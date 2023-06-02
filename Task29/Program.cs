9187fc5ce1543a9f871c00aefc8b57a39bcb8d67
JaneZan <volrymd@gmail.com>
2023-06-02 23:01:14 +0300

    change with void

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,10 +1,15 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 Console.Clear();
+void PrintArray()
+{
 int[] array = new int[8];
 for (int i = 0; i < array.Length; i++)
     {
         Console.WriteLine ("Введите число: ");
         array[i] = int.Parse(Console.ReadLine()!);
-    }
-Console.WriteLine($"Результат: [{string.Join(", ", array)}]");
+         }
+    Console.WriteLine($"Результат: [{string.Join(", ", array)}]");
+};
+
+PrintArray();
